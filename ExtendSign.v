7d5cd012cd784894523a31f2d2d5efc2a1574c97
denporras <denporras07@gmail.com>
2017-11-20 09:49:29 -0600

    [ADD] Project

---
+++ ExtendSign.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:46:16 11/20/2017 
+// Design Name: 
+// Module Name:    ExtendSign 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ExtendSign(
+    input [0:15] immendiate_16b,
+    output [0:32] immendiate_32b
+    );
+
+
+endmodule
