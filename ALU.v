e3d49baf37c8d63ea64b0661b4bbe0f7d7fd8c14
denporras <denporras07@gmail.com>
2017-11-20 22:15:32 -0600

    [ADD] ALU pre_test

---
+++ ALU.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:35:57 11/20/2017 
+// Design Name: 
+// Module Name:    ALU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALU(
+    input [31:0] OperA,
+    input [31:0] OperB,
+    input [1:0] ALU_Code,
+    output [31:0] Result,
+    output Zero,
+    output Negative,
+    output Carry,
+    output Overflow
+    );
+
+
+endmodule
