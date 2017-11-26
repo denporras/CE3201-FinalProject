fb8bf930882d88be117718659098128a7c7ff8fe
denporras <denporras07@gmail.com>
2017-11-26 00:18:59 -0600

    [ADD] branch tested

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -26,10 +26,20 @@ module Instruction_Memory(
 	 reg [31:0] RAM[63:0];
 	 
 	 initial begin
-		RAM[0] = 3818922014;
-		RAM[1] = 3818934276;
-		RAM[2] = 3851755528;
-		RAM[3] = 3850723336;
+		RAM[0] = 32'hE3A00000;
+		RAM[1] = 32'hE3A01001;
+		RAM[2] = 32'hE3A02002;
+		RAM[3] = 32'hE3A04003;
+		RAM[4] = 32'hE3A05000;
+		RAM[5] = 32'hE0800001;
+		RAM[6] = 32'hE0802001;
+		RAM[7] = 32'hE0003000;
+		RAM[8] = 32'hE0003005;
+		RAM[9] = 32'hE5850000;
+		RAM[10] = 32'hE5852004;
+		RAM[11] = 32'hE5952004;
+		RAM[12] = 32'hE1500000;
+		RAM[13] = 32'h0AFFFFF6;
 		end
 	
 	 always @(a)
---
+++ shift_left.v
@@ -0,0 +1,102 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    01:48:35 11/24/2017 
+// Design Name: 
+// Module Name:    shift_left 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module shift(input_shift,shift_amount,type, output_shift);
+input [31:0] input_shift;
+input [4:0] shift_amount;
+input [1:0] type;
+output [31:0] output_shift;
+reg [31:0] output_shift = 32'b0;
+
+always @* begin
+	if(type == 0) begin
+		case(shift_amount)
+			5'b00001: output_shift = input_shift << 1;
+			5'b00010: output_shift = input_shift << 2;
+			5'b00011: output_shift = input_shift << 3;
+			5'b00100: output_shift = input_shift << 4;
+			5'b00101: output_shift = input_shift << 5;
+			5'b00110: output_shift = input_shift << 6;
+			5'b00111: output_shift = input_shift << 7;
+			5'b01000: output_shift = input_shift << 8;
+			5'b01001: output_shift = input_shift << 9;
+			5'b01010: output_shift = input_shift << 10;
+			5'b01011: output_shift = input_shift << 11;
+			5'b01100: output_shift = input_shift << 12;
+			5'b01101: output_shift = input_shift << 13;
+			5'b01110: output_shift = input_shift << 14;
+			5'b01111: output_shift = input_shift << 15;
+			5'b10000: output_shift = input_shift << 16;
+			5'b10001: output_shift = input_shift << 17;
+			5'b10010: output_shift = input_shift << 18;
+			5'b10011: output_shift = input_shift << 19;
+			5'b10100: output_shift = input_shift << 20;
+			5'b10101: output_shift = input_shift << 21;
+			5'b10110: output_shift = input_shift << 22;
+			5'b10111: output_shift = input_shift << 23;
+			5'b11000: output_shift = input_shift << 24;
+			5'b11001: output_shift = input_shift << 25;
+			5'b11010: output_shift = input_shift << 26;
+			5'b11011: output_shift = input_shift << 27;
+			5'b11100: output_shift = input_shift << 28;
+			5'b11101: output_shift = input_shift << 29;
+			5'b11110: output_shift = input_shift << 30;
+			5'b11111: output_shift = input_shift << 31;
+		endcase
+	end
+	else begin
+		case(shift_amount) 
+			5'b00001: output_shift = input_shift >> 1;
+			5'b00010: output_shift = input_shift >> 2;
+			5'b00011: output_shift = input_shift >> 3;
+			5'b00100: output_shift = input_shift >> 4;
+			5'b00101: output_shift = input_shift >> 5;
+			5'b00110: output_shift = input_shift >> 6;
+			5'b00111: output_shift = input_shift >> 7;
+			5'b01000: output_shift = input_shift >> 8;
+			5'b01001: output_shift = input_shift >> 9;
+			5'b01010: output_shift = input_shift >> 10;
+			5'b01011: output_shift = input_shift >> 11;
+			5'b01100: output_shift = input_shift >> 12;
+			5'b01101: output_shift = input_shift >> 13;
+			5'b01110: output_shift = input_shift >> 14;
+			5'b01111: output_shift = input_shift >> 15;
+			5'b10000: output_shift = input_shift >> 16;
+			5'b10001: output_shift = input_shift >> 17;
+			5'b10010: output_shift = input_shift >> 18;
+			5'b10011: output_shift = input_shift >> 19;
+			5'b10100: output_shift = input_shift >> 20;
+			5'b10101: output_shift = input_shift >> 21;
+			5'b10110: output_shift = input_shift >> 22;
+			5'b10111: output_shift = input_shift >> 23;
+			5'b11000: output_shift = input_shift >> 24;
+			5'b11001: output_shift = input_shift >> 25;
+			5'b11010: output_shift = input_shift >> 26;
+			5'b11011: output_shift = input_shift >> 27;
+			5'b11100: output_shift = input_shift >> 28;
+			5'b11101: output_shift = input_shift >> 29;
+			5'b11110: output_shift = input_shift >> 30;
+			5'b11111: output_shift = input_shift >> 31;
+		endcase
+	
+	end
+end
+
+endmodule
